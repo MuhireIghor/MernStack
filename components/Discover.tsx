148059303929daede2a31d1c6d21be768788bfde
muhireIghor <muhireighor123@gmail.com>
2022-07-09 22:15:47 +0200

    Sidebar

---
+++ components/Discover.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+ import type {NextPage} from 'next';
+ import Link from 'next/link';
+ import {useRouter} from 'next/router';
+ import { topics } from '../utils/utils/constants';
+ const Discover:NextPage = ()=>{
+    const router = useRouter();
+    const {topic} = router.query;
+    const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F519997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
+    const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
+    return(
+        <div className='xl:border-2 xl:border-gray-200 pb-6'>
+            <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block '>Popular Topics</p>
+            <div className='flex flex-row gap-3 flex-wrap'>
+                {
+                    topics.map((item)=>
+                        (
+                            <Link href={`/?topic=${item.name}`} key={item.name}>
+                                <div className={topic===item.name?activeTopicStyle:topicStyle}>
+                                <span className='text-2xl font-bold xl:text-md'>{item.icon}</span>
+                                <span className='text-md hidden xl:block font-medium capitalise'>{item.name}</span>
+                                    </div>
+                            </Link>
+                            
+                    )
+
+                    )
+                }
+
+            </div>
+            
+        </div>
+    )
+
+ }
+export default Discover;
---
+++ components/SuggestedAccounts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NextPage} from 'next';
+ const SuggestedAccounts:NextPage = ()=>{
+    return(
+        <div>
+            Suggested Accounts
+        </div>
+    )
+ }
+ export default SuggestedAccounts;
